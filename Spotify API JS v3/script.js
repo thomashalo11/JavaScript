6a3344b864e51c50eb740fa42cbb991b2a581571
thomashalo11 <117899427+thomashalo11@users.noreply.github.com>
2024-03-22 09:04:01 +0100

    Add files via upload

--- Spotify API JS v3/script.js
+++ Spotify API JS v3/script.js
@@ -1,4 +1,4 @@
-var access_token = "BQA98p8JDrnANTvFjyPleTN02u9HB0a6rC0sY10vNEMQS3t6IdbcisHblWOWkuVhWAEAYnon_uhryNZGEaTLPeu06NRqFc_HVF7hQ9rdUh49Pv_2coMLOVYpk5q4ZE5D7524xEpiwUIhblMDa3HeC2H_3T15WJFkUUgBqJZRHGJ7Dy6of1zO2vDNXHgJ69GaFVfdEPSnGU3DXZ7fnCFguGiY-nRiTvE9_Bj_hJdElnrYUg1-W9WSXWtLOq_QUipiMSvsO-b9qg5B15pVI8NQDixAyk56q18QchO1WS-3En8lrZpk5P5kQeC1Ttxm29fD-vG6uQg1QMLh1XatnAyVjVTIlEbqPd-v69SNAJb-UAkbZ4P79l09IsT0e3U0OMektzmwEG54ooa9BtcNQGnChdgVjiUoJxNJUNrOQax5DxzGC2KOpr3xVslJRkzD1d7YDr";
+var access_token = "BQBuoOEblWoNvNLUFbPT8jT-Z5dmorMzePKr-9JbjeePiXmcxPhoaxdIvwxaZw4SpFab7gPm_PMpBYsk-q8a_JCR4OnZ5f0JCvCY8evhYVMZ85Ayq2cOibBaX_j67sjUPHzvzf3v9vCBX2YiA35KW-IqT8Ij2c5xfYLxqCFPnYFxevSVdS1DfiOszOB6hIkp8QLoukJ_jiH4yM9_7X_zXsrn";
 
 async function fetchTracks() {
     try {
@@ -24,9 +24,10 @@ async function renderTable() {
         let tbody = "";
         jsonTracksData['items'].forEach(song => {
             tbody += `<tr><td>${song['track']['name']}</td>`;
-            tbody += `<td>${song['track']['artists'][0]['name']}</td>`;
+            tbody += `<td><a href="${song['track']['artists'][0]['external_urls']['spotify']}">${song['track']['artists'][0]['name']}</a></td>`;
             tbody += `<td><img src="${song["track"]["album"]["images"][0]["url"]}" alt="Copertina" style="width: 100px;" onclick="renderTableAlbum(${song["album"]})"><a href=""</td>`;
-            tbody += `<td><a href="${song["track"]["external_urls"]["spotify"]}">Ascolta</a></tr>`;
+            tbody += `<td><a href="${song["track"]["external_urls"]["spotify"]}">${song['track']['name']}</a>`;
+            tbody += `<td><a href="${song['track']['album']['external_urls']['spotify']}">${song['track']['album']['name']}</a></td></tr>`;
         });
         document.getElementById("tbody").innerHTML = tbody;
     } catch (error) {
