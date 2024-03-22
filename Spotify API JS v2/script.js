f25ac6ede48f05192ae26dd4a4ec1acb8da22647
thomashalo11 <117899427+thomashalo11@users.noreply.github.com>
2024-03-22 09:04:32 +0100

    Add files via upload

--- Spotify API JS v2/script.js
+++ Spotify API JS v2/script.js
@@ -1,4 +1,4 @@
-var access_token = "BQBYrkwzDKTPNY9qr3_XOlziXZR-b9qg5B15pVI8NQDixAyk56q18QchO1WS-3En8lrZpk5P5kQeC1Ttxm29fD-vG6uQg1QMLh1XatnAyVjVTIlEbqPd-v69SNAJb-UAkbZ4P79l09IsT0e3U0OMektzmwEG54ooa9BtcNQGnChdgVjiUoJxNJUNrOQax5DxzGC2KOpr3xVslJRkzD1d7YDr";
+var access_token = "BQBuoOEblWoNvNLUFbPT8jT-Z5dmorMzePKr-9JbjeePiXmcxPhoaxdIvwxaZw4SpFab7gPm_PMpBYsk-q8a_JCR4OnZ5f0JCvCY8evhYVMZ85Ayq2cOibBaX_j67sjUPHzvzf3v9vCBX2YiA35KW-IqT8Ij2c5xfYLxqCFPnYFxevSVdS1DfiOszOB6hIkp8QLoukJ_jiH4yM9_7X_zXsrn";
 
 async function fetchTracks() {
     try {
